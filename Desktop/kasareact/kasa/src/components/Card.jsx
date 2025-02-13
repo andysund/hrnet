2ad3e619a800707b609532d76b240d18b0b8cb0e
enoamaore14120 <121623851+enoamaore14120@users.noreply.github.com>
2025-02-13 16:11:02 +0100

    kasa premier commit

---
+++ Desktop/kasareact/kasa/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import '../styles/HouseCard.css'
+function Card () {
+    return (
+        <div className="Card">
+        <div className="rental-location">
+            Placeholder
+
+        </div>
+            </div>
+        
+        
+     )
+}
+export default Card
---
+++ Desktop/kasareact/kasa/src/components/Housedisplay.jsx
@@ -0,0 +1,17 @@
+import Card from './Card'
+import '../styles/HouseCard.css'
+function Housedisplay () {
+    return (
+        <div className="house-cards">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+            </div>
+        
+        
+     )
+}
+export default Housedisplay
